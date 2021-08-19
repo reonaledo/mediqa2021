ed01b45c54e8763a89212b33fdc4eb671b0ce18a
Dabin Min <reonaledo@gmail.com>
2021-08-19 04:08:03 +0000

    2

---
+++ source/ex2.py
@@ -0,0 +1,17 @@
+"""
+Now load the data line by line
+"""
+from sklearn.model_selection import train_test_split
+
+with open('<path to text file>', 'r') as data:
+  dataset = ["<|title|>" + x.strip() for x in data.readlines()]
+
+train, eval = train_test_split(dataset, train_size=.9, random_state=2020)
+print("training size:" + len(train))
+print("Evaluation size: " + len(eval))
+
+with open('train_tmp.txt', 'w') as file_handle:
+  file_handle.write("<|endoftext|>".join(train))
+
+with open('eval_tmp.txt', 'w') as file_handle:
+  file_handle.write("<|endoftext|>".join(eval))
---
+++ source/preprocessing.py
@@ -0,0 +1,7 @@
+import json
+
+with open('./data/train.json', 'r') as f:
+    train_data = json.load(f)
+
+train_data[0]
+
